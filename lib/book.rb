07d99c5a1710a78e0d2a1677945766beb0d86655
Joshua Hill <josh@jlhworkshop.com>
2019-08-08 16:32:34 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -16,6 +16,14 @@ class Book
     @author
   end
   
+  def page_count
+    @page_count    
+  end
+  
+  def page_count=(num)
+    @page_count = num
+  end
+  
 end
 
 myBook = Book.new("And Then There Were None")
