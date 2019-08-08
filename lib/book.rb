9ea4f02995d30ca7f6dd8e1569031d172b4808b2
Joshua Hill <josh@jlhworkshop.com>
2019-08-08 16:25:12 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -3,7 +3,10 @@ class Book
   def initialize(title, auth, pages, genre)
     @title = title
     end
+    
+    def title
+    end
 end
 
-myBook = Book.new("And Then There Were None",)
+myBook = Book.new("And Then There Were None","Agatha Christie")
 
